b65909fe0f263b6a0839c8b62c3954bbdea92d62
JuanDavinLie <juandavinlie88@gmail.com>
2023-07-11 12:39:31 +0800

    revert middleware

--- middleware.tsx
+++ middleware.tsx
@@ -12,7 +12,7 @@ export async function middleware(req: NextRequest) {
     return res
   } else if (req.nextUrl.pathname.startsWith("/api/integration/shopify/webhooks")) {
     return res
-  } else if (req.nextUrl.pathname.startsWith("/api/integration/shopify/auth/init")) {
+  } else if (req.nextUrl.pathname.startsWith("/api/integration/shopify/auth")) {
     return res
   } 
   const supabase = createMiddlewareSupabaseClient({ req, res })
