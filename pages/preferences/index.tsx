3e3f4ac5bdb5dbc20ef9ab2dcc4c0eb962d48f5f
JuanDavinLie <juandavinlie88@gmail.com>
2023-07-14 13:02:59 +0800

    create preferences page

--- pages/preferences/index.tsx
+++ pages/preferences/index.tsx
@@ -1,19 +1,41 @@
-import { Box } from "@mui/material"
-import { useSupabaseClient } from "@supabase/auth-helpers-react"
+import { Box, Divider, Typography } from "@mui/material"
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react"
 import { useRouter } from "next/router"
 
 const PreferencesPage = () => {
   const supabase = useSupabaseClient()
   const router = useRouter()
-  return (
-    <Box
-      onClick={async () => {
-        const { error } = await supabase.auth.signOut()
-        router.push("/")
-      }}
-    >
-      Logout
+  const user = useUser()
+
+  return user ? (
+    <Box display="flex" flexDirection="column" width="100%" height="100vh">
+      <Box display="flex" p="20px">
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="flex-start"
+          width="70%"
+          maxWidth="500px"
+          gap="30px"
+          boxShadow="2"
+          borderRadius="15px"
+          p="20px"
+        >
+          <Box display="flex" flexDirection="column" gap="5px">
+            <Typography variant="h5">Account Information</Typography>
+            <Divider />
+            <Box display="flex">
+              <Box width="250px">
+                <Typography variant="h6">Email</Typography>
+              </Box>
+              <Typography variant="h6">{user!.email}</Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
     </Box>
+  ) : (
+    "Loading"
   )
 }
 
