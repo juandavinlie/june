80510c7dbfb3e927950a3184fb0874906bb8b90f
JuanDavinLie <juandavinlie88@gmail.com>
2023-09-19 22:50:43 +0800

    fix name bug

--- pages/stores/[storeId]/products/add.tsx
+++ pages/stores/[storeId]/products/add.tsx
@@ -196,7 +196,7 @@ const AddProductPage = () => {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
-            name,
+            name: title,
             description,
             properties: processedProperties,
             variants: processedVariants,
